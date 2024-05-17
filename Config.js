979a7c4575c7e5d6e0500c73117d35f1d0b2aa41
vkureyde <neuncomde@gmail.com>
2024-05-17 09:58:16 +0200

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,24 +3,24 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "16092375565"],
-global.ownername = process.env.OWNER_NAME || "vkurey?",
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = [ process.env.OWNER_NUMBER || "4915678883283"],
+global.ownername = process.env.OWNER_NAME || "NetHunter",
+global.ytname = "YT: ANethunter"
+global.socialm = "GitHub: Nethunter"
+global.location = "Germany"
 
-global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
+global.botname = process.env.BOTNAME || "Nethunter",//name of the bot
 
 //sticker details
-global.stickername = process.env.STICKER || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.stickername = process.env.STICKER || "Nethunter",
 global.packname = 'Sticker By'
-global.author = process.env.AUTHOR || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.author = process.env.AUTHOR || "Nethunter",
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
+global.link = 'Nethunter'
 
 //custom prefix
 global.prefa = process.env.PREFIX || ".",
